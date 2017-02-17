f2ddb3b56561390f05821d399c2a30123b3be7c0
EliakinCosta <eliakim170@gmail.com>
2017-02-17 16:47:26 -0300

    Testing working directory path

--- settings.py
+++ settings.py
@@ -1,7 +1,8 @@
 import os
 
-print('claro', os.path.dirname(os.path.abspath(__file__)))
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+print('claro', BASE_DIR)
 
 
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media")
